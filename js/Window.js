6cb43492ba8f3073c94d014e5beba93afd992b2e
Annie671 <qianyaoliu@dhcp-10-30-37-186.bmcc.cuny.edu>
2015-10-19 15:06:14 -0400

    add new

---
+++ js/Window.js
@@ -0,0 +1,53 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+    //top
+     strokeWeight(2);
+     fill('brown');
+     rect(50,50,300,50);
+
+
+
+    //body
+      rect(100,100,200,300);
+
+      fill('black');
+      rect(110,110,180,280);
+
+       stroke('brown');
+       strokeWeight(3);
+       line(110,250,110+180,250);
+       line(200,110,200,110+280);
+
+    //bottom
+
+       strokeWeight(2);
+         fill('brown');
+       rect(50,400,300,100);
+
+    //plant
+
+        fill('green');
+        ellipse(90,380,50,50);
+
+        fill('green');
+        ellipse(120,380,50,50);
+      fill('green');
+        ellipse(150,380,50,50);
+    fill('green');
+        ellipse(180,380,50,50);
+
+        fill('green');
+        ellipse(210,380,50,50);
+      fill('green');
+        ellipse(240,380,50,50);
+    fill('green');
+        ellipse(270,380,50,50);
+      fill('green');
+        ellipse(300,380,50,50);
+}
+
+
+
+
+
